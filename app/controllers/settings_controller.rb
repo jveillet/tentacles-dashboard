fe24acf5bc069fbdd5e2b8f4f2e9f36d867a9576
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-07-16 19:14:52 +0200

    [ISSUE-11] Create a Settings page

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,6 @@
+class SettingsController < ApplicationController
+  layout 'default'
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy', as: :signout
   get '/auth/failure', to: 'sessions#failure'
+  get '/:username/settings', to: 'settings#index', as: :settings
 end
