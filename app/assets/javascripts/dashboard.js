f499e0c13a68294890eaa78a4a459f7dfbde5092
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-07-16 14:12:00 +0200

    [ISSUE-9] Create the application's Topbar menu

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,2 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$.ready().then(function() { 
+
+  var userName = document.querySelector(".user-name");
+  var menu = document.querySelector('.menu-card');
+
+  userName.addEventListener("click", function(event) {
+    menu.classList.toggle('dropdown-open');
+    clearTimeout(timer)
+    event.preventDefault();
+    return false;
+  });
+
+  menu.addEventListener("mouseout", function(event) {
+    timer = setTimeout(function(event) {
+      menu.classList.remove('dropdown-open');
+    }, 2000);
+  });
+
+  document.addEventListener("keyup", event => {
+    if (event.key === "Escape") {
+      menu.classList.remove('dropdown-open');
+      clearTimeout(timer)
+      return false;
+    }
+  });
+
+});
+
