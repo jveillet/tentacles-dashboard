396585d3b64b3208f2d8b6edb4e92537b6cea025
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-07-23 13:50:42 +0200

    [ISSUE-20] Query the list of repositories

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,53 @@
 class SettingsController < ApplicationController
   layout 'default'
 
+  OrganizationsQuery = Tentacles::Client.parse <<-'GRAPHQL'
+    query($last_orgs:Int!, $last_repos:Int!){
+      viewer {
+        name
+        login
+        avatarUrl
+        url
+        repositories(last: $last_repos) {
+          totalCount
+          nodes {
+            name
+            isPrivate
+          }
+        }
+        organizations(last: $last_orgs) {
+          totalCount
+          nodes {
+            name
+            avatarUrl
+            login
+            url
+            viewerIsAMember
+            repositories(last: $last_repos) {
+              totalCount
+              nodes {
+                name
+                isPrivate
+              }
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
+
   def index
+    results = Tentacles::Client.query(OrganizationsQuery, variables: { last_orgs: 10, last_repos: 100 }, context: client_context)
+    repos = results&.data&.viewer&.repositories&.nodes.map { |repo| repo }
+    results&.data&.viewer&.organizations&.nodes.each do |node|
+      repos << node.repositories.nodes.map { |repo| repo }
+    end
+
+    repos.flatten!
+
+    render 'settings/index', locals: {
+      viewer: results.data.viewer,
+      repos: repos
+    }
   end
 end
