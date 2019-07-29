3dc7d19a4118b4c95de3fa60d663979c13e1d589
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-07-29 17:10:12 +0200

    [ISSUE-28] Query the list of Pull Requests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,5 +7,69 @@ class DashboardController < ApplicationController
   include DashboardHelper
   layout 'default'
 
-  def index; end
+  PullRequestsQuery = Tentacles::Client.parse <<-'GRAPHQL'
+    query($queryString: String!, , $number_or_prs: Int!, $number_or_labels: Int!) {
+      rateLimit {
+        cost
+        remaining
+        resetAt
+       }
+       search(query: $queryString, type: ISSUE, last: $number_or_prs) {
+         edges {
+           node {
+             ... on PullRequest {
+               author {
+                 login
+                 avatarUrl
+                 url
+               }
+               labels(last: $number_or_labels) {
+                 nodes {
+                   color
+                   name
+                   url
+                 }
+               }
+               number
+               url
+               title
+               createdAt
+               state
+               closed
+               headRefName
+               baseRefName
+               commits(last: 1){
+                 nodes {
+                   commit {
+                     commitUrl
+                     abbreviatedOid
+                     status {
+                       state
+                     }
+                   }
+                 }
+               }
+             }
+           }
+         }
+       }
+    }
+  GRAPHQL
+
+  def index
+    pull_requests = []
+
+    unless user_repositories.empty?
+      results = Tentacles::Client.query(
+        PullRequestsQuery,
+        variables: { queryString: build_query_string(user_repositories), number_or_prs: 100, number_or_labels: 10 },
+        context: client_context
+      )
+      pull_requests = results&.data&.search&.edges
+    end
+
+    render 'dashboard/index', locals: {
+      pull_requests: pull_requests
+    }
+  end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
+##
+# Collection of Dashboard helper methods.
+#
 module DashboardHelper
+  def build_query_string(repos)
+    <<~STRING
+      is:pr is:open created:>#{limit_date} #{repos.join(' ') unless repos.empty?} sort:created-asc
+    STRING
+  end
+
+  def limit_date
+    1.year.ago.strftime('%Y-01-01')
+  end
+
+  def user_repositories
+    repos = ENV['GITHUB_REPOS'].to_s
+    repos.split(',')
+  end
 end
