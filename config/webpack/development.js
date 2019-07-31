a181833b737cdf66a78985e7722cbbc3ed17aea6
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-07-31 16:17:03 +0200

    [ISSUE-48] Migrate javascript files to Webpack

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,7 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-console.log(environment.output_path)
-console.log(environment.source_path)
-
 module.exports = environment.toWebpackConfig()
