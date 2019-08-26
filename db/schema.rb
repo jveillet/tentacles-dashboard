726317c2cb298992e1bae89f8863793c6340c4dd
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-08-26 16:17:29 +0200

    Add settings migration into users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_124509) do
+ActiveRecord::Schema.define(version: 2019_08_26_124007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_124509) do
     t.string "uid", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "settings"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
